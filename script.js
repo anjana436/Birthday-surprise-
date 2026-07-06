db002ba73f7907fb07a54524330046d6eacf1e36
anjana436 <ouijaanjana@gmail.com>
2026-07-06 15:04:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("giftBtn").onclick = function () {
+    document.getElementById("surprise").style.display = "block";
+    this.style.display = "none";
+};
+
+document.getElementById("cakeBtn").onclick = function () {
+    document.getElementById("message").style.display = "block";
+    alert("🎉 Happy Birthday Kathirvel ❤️");
+};
